6d143a5fab420a977c6286989e77b70613a4db99
Eric Li <eric@artela.network>
2023-08-07 17:13:28 +0800

    fix:add protogen for artela proto files

---
+++ scripts/protocgen.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# --------------
+# Commands to run locally
+# docker run --network host --rm -v $(CURDIR):/workspace --workdir /workspace tendermintdev/sdk-proto-gen:v0.7 sh ./protocgen.sh
+#
+set -eo pipefail
+
+echo "Generating gogo proto code"
+proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+for dir in $proto_dirs; do
+  proto_files=$(find "${dir}" -maxdepth 1 -name '*.proto')
+  for file in $proto_files; do
+    # Check if the go_package in the file is pointing to evmos
+    if grep -q "option go_package.*artela" "$file"; then
+      buf generate --template proto/buf.gen.gogo.yaml "$file"
+    fi
+  done
+done
+
+# move proto files to the right places
+cp -r github.com/artela-network/artela/v*/* ./
+rm -rf github.com
