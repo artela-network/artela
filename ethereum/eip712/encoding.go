961c94ebf25a649f1e7ffb2ad892fe91f9bbad7b
yuan <fatfishman90@gmail.com>
2023-08-08 15:56:05 +0800

    feat: go.mod init success.

--- ethereum/eip712/encoding.go
+++ ethereum/eip712/encoding.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/cosmos/cosmos-sdk/simapp/params"
+	//"github.com/cosmos/cosmos-sdk/simapp/params"
 	"github.com/cosmos/cosmos-sdk/x/auth/migrations/legacytx"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
