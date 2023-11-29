9cc0841b650039032605f32db948587bb55b89c3
yuan <fatfishman90@gmail.com>
2023-11-29 10:58:13 +0800

    fix: update aartela to uart, artela to art.

--- ethereum/types/coin.go
+++ ethereum/types/coin.go
@@ -16,10 +16,10 @@ const (
 	// - Governance parameters: denomination used for spam prevention in proposal deposits
 	// - Crisis parameters: constant fee denomination used for spam prevention to check broken invariant
 	// - EVM parameters: denomination used for running EVM states transitions in Artela.
-	AttoArtela string = "aartela"
+	AttoArtela string = "uart"
 
 	// BaseDenomUnit defines the base denomination unit for Artela.
-	// 1 artela = 1x10^{BaseDenomUnit} aartela
+	// 1 art = 1x10^{BaseDenomUnit} uart
 	BaseDenomUnit = 18
 
 	// DefaultGasPrice is default gas price for evm transactions
@@ -29,19 +29,19 @@ const (
 // PowerReduction defines the default power reduction value for staking
 var PowerReduction = sdkmath.NewIntFromBigInt(new(big.Int).Exp(big.NewInt(10), big.NewInt(BaseDenomUnit), nil))
 
-// NewArtelaCoin is a utility function that returns an "aartela" coin with the given sdkmath.Int amount.
+// NewArtelaCoin is a utility function that returns an "uart" coin with the given sdkmath.Int amount.
 // The function will panic if the provided amount is negative.
 func NewArtelaCoin(amount sdkmath.Int) sdk.Coin {
 	return sdk.NewCoin(AttoArtela, amount)
 }
 
-// NewArtelaDecCoin is a utility function that returns an "aartela" decimal coin with the given sdkmath.Int amount.
+// NewArtelaDecCoin is a utility function that returns an "uart" decimal coin with the given sdkmath.Int amount.
 // The function will panic if the provided amount is negative.
 func NewArtelaDecCoin(amount sdkmath.Int) sdk.DecCoin {
 	return sdk.NewDecCoin(AttoArtela, amount)
 }
 
-// NewArtelaCoinInt64 is a utility function that returns an "aartela" coin with the given int64 amount.
+// NewArtelaCoinInt64 is a utility function that returns an "uart" coin with the given int64 amount.
 // The function will panic if the provided amount is negative.
 func NewArtelaCoinInt64(amount int64) sdk.Coin {
 	return sdk.NewInt64Coin(AttoArtela, amount)
