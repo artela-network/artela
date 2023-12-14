481ec1c452b4f05e9b8e6f084272afb4d164578c
yuan <fatfishman90@gmail.com>
2023-12-14 17:43:19 +0800

    fix: update ethereum account and tx type.

--- ethereum/types/web3.pb.go
+++ ethereum/types/web3.pb.go
@@ -71,7 +71,7 @@ func (m *ExtensionOptionsWeb3Tx) XXX_DiscardUnknown() {
 var xxx_messageInfo_ExtensionOptionsWeb3Tx proto.InternalMessageInfo
 
 func init() {
-	proto.RegisterType((*ExtensionOptionsWeb3Tx)(nil), "artela.types.v1.ExtensionOptionsWeb3Tx")
+	proto.RegisterType((*ExtensionOptionsWeb3Tx)(nil), "ethermint.types.v1.ExtensionOptionsWeb3Tx")
 }
 
 func init() { proto.RegisterFile("artela/types/v1/web3.proto", fileDescriptor_1336ae929ddce688) }
