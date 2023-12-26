64bacd64570feecfcf89c120937df9aef02bef0f
yuan <fatfishman90@gmail.com>
2023-12-26 17:52:47 +0800

    fix: update denom aartela to uart.

--- ethereum/server/start.go
+++ ethereum/server/start.go
@@ -212,7 +212,7 @@ is performed. Note, when enabled, gRPC will also be automatically enabled.
 	cmd.Flags().StringSlice(artelaflag.JSONRPCAPI, config.GetDefaultAPINamespaces(), "Defines a list of JSON-RPC namespaces that should be enabled")
 	cmd.Flags().String(artelaflag.JSONRPCAddress, config.DefaultJSONRPCAddress, "the JSON-RPC server address to listen on")
 	cmd.Flags().String(artelaflag.JSONWsAddress, config.DefaultJSONRPCWsAddress, "the JSON-RPC WS server address to listen on")
-	cmd.Flags().Uint64(artelaflag.JSONRPCGasCap, config.DefaultGasCap, "Sets a cap on gas that can be used in eth_call/estimateGas unit is aartela (0=infinite)")     //nolint:lll
+	cmd.Flags().Uint64(artelaflag.JSONRPCGasCap, config.DefaultGasCap, "Sets a cap on gas that can be used in eth_call/estimateGas unit is uart (0=infinite)")        //nolint:lll
 	cmd.Flags().Float64(artelaflag.JSONRPCTxFeeCap, config.DefaultTxFeeCap, "Sets a cap on transaction fee that can be sent via the RPC APIs (1 = default 1 artela)") //nolint:lll
 	cmd.Flags().Int32(artelaflag.JSONRPCFilterCap, config.DefaultFilterCap, "Sets the global cap for total number of filters that can be created")
 	cmd.Flags().Duration(artelaflag.JSONRPCEVMTimeout, config.DefaultEVMTimeout, "Sets a timeout used for eth_call (0=infinite)")
