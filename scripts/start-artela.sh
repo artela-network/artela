cadc5c0d098b7bbaa2f230d565558bd8e905803e
luketheart <eric@artela.network>
2024-04-16 17:05:26 +0800

    fix: use iavl-1.1.2

--- scripts/start-artela.sh
+++ scripts/start-artela.sh
@@ -8,17 +8,11 @@ sed -i 's/"extra_eips": \[\]/"extra_eips": \[3855\]/g' $DATA_DIR/config/genesis.
 
 echo "starting artela node $i in background ..."
 ./artelad start \
---log_level debug \
 --minimum-gas-prices=0.0001art \
 --api.enable \
 --json-rpc.api eth,txpool,personal,net,debug,web3,miner \
 --api.enable \
 &>>$DATA_DIR/node.log & disown
-
-pid=$(ps ax | grep '[a]rtelad' | awk '{print $1}')
-
-nohup dlv attach $pid --listen=:19211 --headless=true --log=true  \
-  --log-output=debugger,debuglineerr,gdbwire,lldbout,rpc --accept-multiclient --api-version=2 --continue &> /dev/null &
 
 echo "started artela node"
 tail -f /dev/null
