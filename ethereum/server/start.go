73cc14bd4179fd2d75051ab344599e2e450389f5
Luke <129730838+LukeNinjaX@users.noreply.github.com>
2024-06-28 17:34:49 +0800

    fix: call db compaction (#128)

--- ethereum/server/start.go
+++ ethereum/server/start.go
@@ -59,14 +59,15 @@ const (
 	FlagTrace              = "trace"
 	FlagInvCheckPeriod     = "inv-check-period"
 
-	FlagPruning             = "pruning"
-	FlagPruningKeepRecent   = "pruning-keep-recent"
-	FlagPruningInterval     = "pruning-interval"
-	FlagIndexEvents         = "index-events"
-	FlagMinRetainBlocks     = "min-retain-blocks"
-	FlagIAVLCacheSize       = "iavl-cache-size"
-	FlagDisableIAVLFastNode = "iavl-disable-fastnode"
-	FlagIAVLLazyLoading     = "iavl-lazy-loading"
+	FlagPruning              = "pruning"
+	FlagPruningKeepRecent    = "pruning-keep-recent"
+	FlagPruningInterval      = "pruning-interval"
+	FlagIndexEvents          = "index-events"
+	FlagMinRetainBlocks      = "min-retain-blocks"
+	FlagIAVLCacheSize        = "iavl-cache-size"
+	FlagDisableIAVLFastNode  = "iavl-disable-fastnode"
+	FlagIAVLLazyLoading      = "iavl-lazy-loading"
+	FlagForceCompactInterval = "force-compact-interval"
 
 	// states sync-related flags
 	FlagStateSyncSnapshotInterval   = "state-sync.snapshot-interval"
