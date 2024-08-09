bd8a83534a35b5175c1644af2d085483ba60366c
dumbeng <jack@artela.network>
2024-08-09 15:59:39 +0800

    fix: re-enable v1 store

--- x/aspect/store/v1/init.go
+++ x/aspect/store/v1/init.go
@@ -1,7 +1,9 @@
 package v1
 
+import "github.com/artela-network/artela/x/aspect/store"
+
 func init() {
-	//store.RegisterAccountStore(1, NewAccountStore)
-	//store.RegisterAspectMetaStore(1, NewAspectMetaStore)
-	//store.RegisterAspectStateStore(1, NewStateStore)
+	store.RegisterAccountStore(1, NewAccountStore)
+	store.RegisterAspectMetaStore(1, NewAspectMetaStore)
+	store.RegisterAspectStateStore(1, NewStateStore)
 }
